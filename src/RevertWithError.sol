3ec22929017223bd8e193a4ac34877fcd3a8a45e
lcfr <me@lcfr.io>
2025-02-01 18:06:50 -0500

    RevertWithError

--- src/RevertWithError.sol
+++ src/RevertWithError.sol
@@ -7,6 +7,13 @@ contract RevertWithError {
             // revert the function with an error of type `Error(string)`
             // use "RevertRevert" as error message
             // Hint: The error type is a predefined four bytes. See https://www.rareskills.io/post/try-catch-solidity
+
+
+	    mstore(0x00, 0x08c379a0)
+	    mstore(0x20, 0x20)
+	    mstore(0x40, 0x0C)
+	    mstore(0x60, 0x5265766572745265766572740000000000000000000000000000000000000000)
+	    revert(0x1c, 0x64)
         }
     }
 }
