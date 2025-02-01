a4db59a0d75bbecd5bd78be5bd3c0d755adaf866
lcfr <me@lcfr.io>
2025-02-01 18:14:09 -0500

    Double

--- src/Double.sol
+++ src/Double.sol
@@ -13,6 +13,10 @@ contract Double {
           // hint: x can be directly accessed in assembly
 
           // see here for how to multiply in YUL: https://docs.soliditylang.org/en/latest/yul.html#evm-dialect
+
+	  mstore(0x00, mul(x, 0x2))
+	  return(0x00, 0x20)
+
       }
   }
 }
