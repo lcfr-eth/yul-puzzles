82368282ad59f271e473335e25ddf38951676ea1
lcfr <me@lcfr.io>
2025-02-01 20:14:55 -0500

    ReturnString

--- src/ReturnString.sol
+++ src/ReturnString.sol
@@ -6,6 +6,12 @@ contract ReturnString {
         assembly {
             // your code here
             // return the exact string: `Hello, RareSkills`
-        }
+
+	    mstore(0x00, 0x20)
+            mstore(0x20, 17)
+	    mstore(0x40, 0x48656c6c6f2c2052617265536b696c6c73000000000000000000000000000000)
+	    return(0x00, 0x60)
+
+      }
     }
 }
